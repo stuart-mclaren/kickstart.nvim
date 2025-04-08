a6463bfe2b66d0543269b92e8fa5b4cf11a17b05
Stuart McLaren <8805659+stuart-mclaren@users.noreply.github.com>
2025-04-08 18:17:04 +0100

    Add TWS command
    Use ':TWS' to remove Trailing White Space

--- init.lua
+++ init.lua
@@ -190,6 +190,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+
+-- mclaren
+vim.api.nvim_create_user_command('TWS', function()                              
+  vim.cmd [[%s/\s\+$//e]]                                                       
+end, {}) 
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
